b1f9a3e8e87c856a6e394c33638bbd86c4b366db
Alessandro Vendramini <alessandro.vendramini.developer@gmail.com>
2024-05-24 09:42:49 +0200

    add theme and colors

--- app/src/main/java/alessandro/vendramini/hackernews/MainActivity.kt
+++ app/src/main/java/alessandro/vendramini/hackernews/MainActivity.kt
@@ -1,9 +1,9 @@
 package alessandro.vendramini.hackernews
 
+import alessandro.vendramini.hackernews.ui.theme.HackerNewsTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,12 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import alessandro.vendramini.hackernews.ui.theme.HackerNewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // enableEdgeToEdge()
         setContent {
             HackerNewsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
